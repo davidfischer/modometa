1a91fd35e04eefd836305bbda3617a758fe24fe4
David Fischer <djfische@gmail.com>
2026-09-08 22:28:55 -0700

    More performance
    - Improve deck and archetype detail views
    - Instruct well-behaved bots to ignore most of the site

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,6 @@
 """WSGI config for Modometa."""
 
+import logging
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -7,3 +8,13 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 application = get_wsgi_application()
+
+# Pre-warm kNN index on server startup so initial deck view requests do not incur cold-load penalty
+try:
+    from core.engine.knn import get_global_knn_index
+
+    get_global_knn_index()
+except Exception as exc:
+    logging.getLogger("core.knn").warning(
+        "Failed to pre-warm kNN index on WSGI startup: %s", exc
+    )
